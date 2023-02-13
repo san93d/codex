34319a07fdc67f082c3f062d06d9d8a55b391493
Santosh Adhikari <san_adhikari@yahoo.com>
2023-02-13 22:26:26 +1100

    Updated node server url

--- client/script.js
+++ client/script.js
@@ -86,7 +86,9 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('http://localhost:5000', {
+    
+    //Testing : http://localhost:5000
+    const response = await fetch('https://codex-cq8z.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
